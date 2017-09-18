8aa58500bf36fc4230ea66566a9637a85e45358c
Kevin Kauffman <dokkevin@gmail.com>
2017-09-18 18:17:19 -0400

    Some Changes for Title and Char

--- character/Character.java
+++ character/Character.java
@@ -1,13 +1,15 @@
 /*
-SE Project: SPORK
-Authors: Kevin Kauffman, Glenn Sweithelm
-Character - Creates a singleton character and handles the stats for the player's character created
-Change Log
-////////////////////////////////////////////////////////////////////////////////
-Date       Contributer    Change
-17Sept17    Glenn         First draft completed and ready to be verified
+ * SE Project: SPORK
+ * Authors: Kevin Kauffman, Glenn Sweithelm
+ * Character - Creates a singleton character and handles the stats for the
+ * player's character created
+ * Change Log
+ * ////////////////////////////////////////////////////////////////////////////
+ * Date       Contributer    Change
+ * 17Sept17   Glenn          First draft completed and ready to be verified
 */
-package sporkcreatecharacter;
+
+package character;
 
 import static javafx.scene.text.Font.font;
 import javafx.geometry.Insets;
@@ -24,43 +26,42 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Character {
-    private static Character player = new Character();              //Instantiates Singleton
-    static GridPane createChar = new GridPane();                    //All this stuff is the UI elements
+    //Instantiates Singleton
+    private static Character player = new Character();
+
+    //All this stuff is the UI elements
+    static GridPane createChar = new GridPane();
     static Label lUsername = new Label("NAME");
     static Label lClass = new Label("CLASS");
-    static Label lGender = new Label ("GENDER");
     static Scene scene = new Scene(createChar, 1500, 680);
     static TextField tfUsername = new TextField();
     static TextArea taStats = new TextArea();
     static RadioButton rbWarrior = new RadioButton("Warrior");
-    static RadioButton rbMage = new RadioButton("Mage");
-    static RadioButton rbArcher = new RadioButton("Archer");
-    static RadioButton rbRogue = new RadioButton("Rogue");
-    static RadioButton rbMale = new RadioButton("Male");
-    static RadioButton rbFemale = new RadioButton("Female");
     static Button btCreate = new Button("Start New Game");
-    
+
     //Player's actual stats. Seperated from the interface items
     private static int hp;
     private static int defense;
-    private static int stamina;
     private static int attack;
-    private static int magicAttack;
+    private static int exp;
     private static String username;
-    private static String gender;
-    
+    private static String class;
+
     //constructor for the Singleton. The stats will vary, so begins with nothing
     private Character(){
         hp = 0;
         defense = 0;
+        attack = 0;
+        exp = 0;
         username = "";
+        class = "Warrior" // TODO: To be changed when we add more class
     }
-    
+
     //singleton function to get instance
     public static Character getInstance(){
         return player;
     }
-    
+
     //sets the UI up for Creating character and game
     public static void setSceneCharacter(Stage stage){
         setStyles();
@@ -77,34 +78,34 @@ public class Character {
         createChar.add(rbArcher, 4, 2);
         createChar.add(taStats, 1, 3, 4, 1);
         createChar.add(btCreate, 0, 4, 2, 1);
-        
-        
+
+
         //This makes radio buttons interactable when clicked, though arrow keys still need to be programmed.
         //  It's a start for the place holders
-        
+
         rbWarrior.setOnAction(e -> {
             taStats.setText("Warrior \nStrong and hardy, but lacking in specialty skills \n\nHealth: 10 \nStamina: 10 \n"
                 + "Attack Power: 5 \nMagic Power: 1");
         });
-        
+
         rbMage.setOnAction(e ->{
             taStats.setText("Mage \nIntelligent and powerful, bending the elements within their hands \n\nHealth: 6 \n"
                     + "Stamina: 8 \nAttackPower: 2 \nMagic Power: 8");
         });
-        
+
         rbRogue.setOnAction(e -> {
             taStats.setText("Rogue \nSwift and nimble, able to dodge attacks and sneak by enemies \n\nHealth: 8\n"
                     + "Stamina: 12 \nAttack Power: 3 \nMagic Power: 2");
         });
-        
+
         rbArcher.setOnAction(e -> {
             taStats.setText("Archer \nAgile, using ranged to their advantage, though not in the magical sense like the mage \n\n"
                     + "Health: 8 \nStamina: 10 \nAttack Power: 4 \nMagic Power: 3");
         });
-        
+
         //sets all the character values when button is clicked, but only if a name is entered
         //" " works as a name, so may need to find how to solve that
-        
+
         btCreate.setOnAction(e -> {
             if(tfUsername.getText().length() < 1){
                 errorMessage();
@@ -112,14 +113,14 @@ public class Character {
             else{
                 setUsername(tfUsername.getText());
                 setDefense(1);
-                
+
                 if(rbMale.isSelected()){
                     setGender("Male");
                 }
                 else{
                     setGender("Female");
                 }
-                
+
                 if(rbWarrior.isSelected()){
                     setHp(10);
                     setStamina(10);
@@ -148,11 +149,11 @@ public class Character {
             }
         });
     }
-    
+
     //I am just about positive there is the ability to apply CSS stylesheets to elements as you would html,
     //  though I have never been succesful in getting it figured out. As such, this could be done so much better
     //  once it is figured out.
-    
+
     public static void setStyles(){
         createChar.setPadding(new Insets(40, 40, 100, 100));      //Page Margins (top, bottom, right, left)
         createChar.setVgap(50);
@@ -175,20 +176,20 @@ public class Character {
         taStats.setText("Warrior \nStrong and hardy, but lacking in specialty skills \n\nHealth: 10 \nStamina: 10 \n"
                 + "Attack Power: 5 \nMagic Power: 1");          //These are place holders until we talk about fighting
                                                                 //Since warrior is default, it can start with this text
-        
+
         ToggleGroup classGroup = new ToggleGroup();             //So only one class/gender can be selected
         rbWarrior.setToggleGroup(classGroup);
         rbMage.setToggleGroup(classGroup);
         rbRogue.setToggleGroup(classGroup);
         rbArcher.setToggleGroup(classGroup);
         rbWarrior.setSelected(true);                            //Default selected
-        
+
         ToggleGroup genderGroup = new ToggleGroup();
         rbMale.setToggleGroup(genderGroup);
         rbFemale.setToggleGroup(genderGroup);
         rbMale.setSelected(true);
     }
-    
+
     //sets up a new stage that displays a message to enter username
     public static void errorMessage(){
         Stage error = new Stage();
@@ -196,79 +197,79 @@ public class Character {
         Scene ErrorScene = new Scene(errorPane, 300, 100);
         Text tError = new Text("Please Enter a Username");
         Button ok = new Button("Okay");
-        
+
         error.setResizable(false);
         ok.setPrefSize(100, 30);
         errorPane.setVgap(10);
         errorPane.setAlignment(Pos.CENTER);
         tError.setFont(font("Imprint MT Shadow", 16));
         ok.setFont(font("Imprint MT Shadow", 16));
-        
+
         errorPane.add(tError, 0 , 0, 3, 1);
         errorPane.add(new Label("\t"), 0, 1);
         errorPane.add(ok, 1, 1);
         error.setTitle("Username");
         error.setScene(ErrorScene);
         error.show();
-        
+
         ok.setOnAction(e -> {
             error.close();
         });
     }
-    
+
     //all getters and setters
     static int getHp(){
         return hp;
     }
-    
+
     static int getStamina(){
         return stamina;
     }
-    
+
     static int getDefense(){
         return defense;
     }
-    
+
     static int getAttack(){
         return attack;
     }
-    
+
     static int getMagicAttack(){
         return magicAttack;
     }
-    
+
     static String getName(){
         return username;
     }
-    
+
     static String getGender(){
         return gender;
     }
-    
+
     static void setHp(int number){
         hp = number;
     }
-    
+
     static void setStamina(int number){
         stamina = number;
     }
-    
+
     static void setDefense(int number){
         defense = number;
     }
-    
+
     static void setAttack(int number){
         attack = number;
     }
-    
+
     static void setMagicAttack(int number){
         magicAttack = number;
     }
-    
+
     static void setUsername(String name){
         username = name;
     }
-    
+
     static void setGender(String _gender){
         gender = _gender;
     }
--- title/SporkTitleScreen.java
+++ title/SporkTitleScreen.java
@@ -1,9 +1,15 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package sporktitlescreen;
+ * SE Project: SPORK
+ * Authors: Kevin Kauffman, Glenn Sweithelm
+ * Character - Creates a singleton character and handles the stats for the
+ * player's character created
+ * Change Log
+ * ////////////////////////////////////////////////////////////////////////////
+ * Date       Contributer    Change
+ * 17Sept17   Glenn          First draft completed and ready to be verified
+*/
+
+package title;
 
 import javafx.application.Application;
 import javafx.geometry.Pos;
@@ -14,58 +20,54 @@ import javafx.scene.layout.GridPane;
 import static javafx.scene.text.Font.font;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Glenn
- */
 public class SporkTitleScreen extends Application {
+   public static void main(String[] args) {
+      launch(args);
+   }
+
     GridPane root = new GridPane();
     Scene scene = new Scene(root, 1500, 680);
     Label sporkTitle = new Label("SPORK");
     Button newGame = new Button("New Game");
     Button loadGame = new Button("Load Game");
     Button options = new Button("Options");
-    
+
     @Override
     public void start(Stage primaryStage) {
         setStyles();
-        root.add(sporkTitle, 0, 0, 2, 1);       
+        root.add(sporkTitle, 0, 0, 2, 1);
         root.add(new Label("\t\t\t\t\t"), 0, 1);           //Adds space for the buttons to be centered.
         root.add(new Label("\t\t\t\t\t"), 0, 2);           //Problem is, this way to do it sucks. Gotta get that CSS figured out.
         root.add(new Label("\t\t\t\t\t"), 0, 3);
         root.add(newGame, 1, 1);
         root.add(loadGame, 1, 2);
         root.add(options, 1, 3);
-        
+
         primaryStage.setTitle("Spork");
         primaryStage.setScene(scene);
         primaryStage.show();
-        
+
         newGame.setOnAction(e -> {
             //This will initiate Creating the Character
         });
-        
+
         loadGame.setOnAction(e -> {
             //This will initiate Loading a save
         });
-        
+
         options.setOnAction(e -> {
             //Probably just pop up a menu to choose some basic stuff like difficulty, which then could
             //  be deserialized (yay! :) ) on load-up.
         });
     }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
-    
+
     //I am just about positive there is ways to get stylesheets with .CSS properties instead
     //    of styling the stage this really long, code-heavy way, so this could very well probably
     //    be able to be done better later. I just never got CSS to work in javaFX.
-    
+
     //Also, there is limited styles you can do this way, so there has to be a CSS way to make things look better.
-    
+
     public void setStyles(){
         root.setAlignment(Pos.TOP_CENTER);
         root.setVgap(20);
