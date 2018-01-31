ca0a45b74d1a01dc6ced54dd5e67bce6a2ec8658
Kevin Kauffman <dokkevin@gmail.com>
2018-01-31 12:28:09 -0500

    Added Paremeter Checking to Character Setters
    Added min and max variables for abstraction and testing. 
    Added parameter checking to setters based on those variables.
    Added exp getter and setter because it was missing
    Next Steps: Update JUnit tests for character. 
    Add tests to ensure cases outside parameters are behaving correctly. 
    Add exp tests since they will be missing.

--- src/character/Character.java
+++ src/character/Character.java
@@ -2,11 +2,12 @@
  * SE Project: SPORK
  * Authors: Kevin Kauffman, Glenn Sweithelm
  * Character - Creates a singleton character and handles the stats for the
- * player's character created
+ *             player's character created
  * Change Log
- * ////////////////////////////////////////////////////////////////////////////
+ * /////////////////////////////////////////////////////////////////////////////
  * Date       Contributer    Change
  * 18Jan18    Kevin          Seperated CharacterGUI and Character Class
+ * 31Jan18    Kevin          Determined Parameters and Added Checking to Setters
 */
 
 package character;
@@ -15,7 +16,7 @@ public class Character {
     //Instantiates Singleton
     private static Character player = new Character();
     
-    //Player's actual stats. Seperated from the interface items
+    //Player's actual stats.
     private int hp;
     private int defense;
     private int attack;
@@ -23,14 +24,28 @@ public class Character {
     private String username;
     private String job; //These are classes
     
+    // Player's min and max stats. May change depending on future development.
+    // For use in this class' setters and for testing.
+    // Made public to accomodate testing without using creating getters.
+    // Made final to disallow being set by other classes.
+    public final int minHp = 0;
+    public final int maxHp = 10;
+    public final int minDef = 1;
+    public final int maxDef = 10;
+    public final int minAtt = 1;
+    public final int maxAtt = 10;
+    public final int minExp = 0; // These two will be dependant on level
+    public final int maxExp = 10; // Will change when development gets to level
+    
     //constructor for the Singleton. The stats will vary, so begins with nothing
+    // Parameters will change as development continues
     private Character(){
-        hp = 0;
-        defense = 0;
-        attack = 0;
-        exp = 0;
+        hp = minHp;
+        defense = minDef;
+        attack = minAtt;
+        exp = minExp;
         username = "";
-        job = "Warrior"; // TODO: To be changed when we add more class
+        job = "Chef"; // TODO: To be changed when we add more class
     }
     
     //singleton function to get instance
@@ -39,7 +54,6 @@ public class Character {
     }
     
     //all getters and setters
-    // TO DO: Determine Parameters for these and check for them in setters.
     public int getHp(){
         return hp;
     }
@@ -55,20 +69,60 @@ public class Character {
     public String getUsername(){
         return username;
     }
+    
+    public int getExp(){
+        return exp;
+    }
 
+    //To Change Min and Max values, see variables at top of class.
     public void setHp(int number){
-        hp = number;
+        if(number < minHp){
+            hp = minHp;
+        } else if (number > maxHp){
+            hp = maxHp;
+        } else {
+            hp = number;
+        }
     }
 
+    //To Change Min and Max values, see variables at top of class.
     public void setDefense(int number){
-        defense = number;
+        if(number < minDef){
+            defense = minDef;
+        } else if (number > maxDef){
+            defense = maxDef;
+        } else {
+            defense = number;
+        }
     }
 
+    //To Change Min and Max values, see variables at top of class.
     public void setAttack(int number){
-        attack = number;
+        if(number < minAtt){
+            attack = minAtt;
+        } else if (number > maxAtt){
+            attack = maxAtt;
+        } else {
+            attack = number;
+        }
+    }
+    
+    //To Change Min and Max values, see variables at top of class.
+    public void setExp(int number){
+        if(number < minExp){
+            exp = minExp;
+        } else if (number > maxExp){
+            exp = maxExp;
+        } else {
+            exp = number;
+        }
     }
 
     public void setUsername(String name){
+        // Only to be called when usernameIsGood method is used.
         username = name;
     }
+    
+    //TODO: include decrement / increment functions so that combat doesn't have
+    //      to rely simply on getters.
 }
