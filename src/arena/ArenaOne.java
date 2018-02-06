f852ad496df68c91b6fb888cc0ea71b0916c3843
Kevin Kauffman <dokkevin@gmail.com>
2018-02-06 08:11:16 -0500

    Added Generic Obstacle
    Needs major cleaning

--- src/arena/ArenaOne.java
+++ src/arena/ArenaOne.java
@@ -9,6 +9,8 @@
  * 24jan18    Kevin          Changed from Animation_Demo to ArenaOne and created
  *                           background and adjusted boundaries.
  * 30Jan18    Kevin          Made Stage FullScreen & Fixed Boundaries
+ * 06Feb18    Kevin          Made ball size and position dynamic
+ *                           Added fixed, generic obstacle - to be dynamic later
 */
 
 package arena;
@@ -22,29 +24,25 @@ import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class ArenaOne {
-    boolean pressed = false;
-    
    public static void start(Stage stage) {
-      //Drawing a Circle
-      Circle circle = new Circle();
+      //Drawing a Circle (representing the playable character)
+      Circle chara = new Circle();
+      chara.setCenterX(Toolkit.getDefaultToolkit().getScreenSize().getWidth() * 0.5);
+      chara.setCenterY(Toolkit.getDefaultToolkit().getScreenSize().getHeight() * 0.5);
+      chara.setRadius(Toolkit.getDefaultToolkit().getScreenSize().getHeight() * 0.025);
+      chara.setFill(Color.BROWN);
+      chara.setStrokeWidth(0.0);
+      
+      //Drawing a Circle (representing an obstacle)
+      Circle obs = new Circle();
+      obs.setCenterX(Toolkit.getDefaultToolkit().getScreenSize().getWidth() * 0.40);
+      obs.setCenterY(Toolkit.getDefaultToolkit().getScreenSize().getHeight() * 0.50);
+      obs.setRadius(Toolkit.getDefaultToolkit().getScreenSize().getHeight() * 0.05);
+      obs.setFill(Color.BLUE);
+      obs.setStrokeWidth(0.0);
 
-      //Setting the position of the circle
-      circle.setCenterX(350.0f);
-      circle.setCenterY(300.0f);
-
-      //Setting the radius of the circle
-      circle.setRadius(20.0f);
-
-      //Setting the color of the circle
-      circle.setFill(Color.BROWN);
-
-      //Setting the stroke width of the circle
-      circle.setStrokeWidth(20);
-
-
-
-      //Creating a Group object
-      Pane root = new Pane(circle);
+      //Creating a Pane object
+      Pane root = new Pane(chara);
 
       //Creating a scene object
       Scene scene = new Scene(root, 600, 300);
@@ -52,43 +50,45 @@ public class ArenaOne {
       //Setting title to the Stage
       stage.setTitle("Spork");
       
-
       //Adding scene to the stage
       stage.setScene(scene);
-      
       scene.getStylesheets().add(ArenaOne.class.getResource("ArenaOne.css").toExternalForm());
       root.getStyleClass().add("arena");
-      
       stage.setFullScreen(true);
-
+      
+      // Add obstacle to pane
+      root.getChildren().addAll(obs);
+      
       scene.setOnKeyPressed(e -> {
           //TODO look into replacing if statement with SWITCH statement
+          //TODO Make Obstacle Checking dynamic (and add function(s) for checking pos / obstacles)
+          //TODO add functions to get char & obs extremes so we can clean this up.
           if (e.getCode() == KeyCode.LEFT)
           {
-            if(circle.getCenterX() - circle.getRadius() > (Toolkit.getDefaultToolkit().getScreenSize().getWidth() * 0.145))
+            if((chara.getCenterX() - chara.getRadius()) > (Toolkit.getDefaultToolkit().getScreenSize().getWidth() * 0.145) && ((chara.getCenterX() - chara.getRadius()) > (obs.getCenterX() + obs.getRadius() + 1.0) || ((chara.getCenterY() + chara.getRadius()) < (obs.getCenterY() - obs.getRadius())) || ((chara.getCenterY() - chara.getRadius()) > (obs.getCenterY() + obs.getRadius())) || ((chara.getCenterX() + chara.getRadius()) < (obs.getCenterX() - obs.getRadius()))))
             {
-                circle.setCenterX(circle.getCenterX() - 14.0);
+                chara.setCenterX(chara.getCenterX() - 1.0); // Changed movement to 1px at a time because it caused an issue with the obstacle
             }
           }
           else if (e.getCode() == KeyCode.RIGHT)
           {
-            if(circle.getCenterX() + circle.getRadius() < Toolkit.getDefaultToolkit().getScreenSize().getWidth() - (Toolkit.getDefaultToolkit().getScreenSize().getWidth() * 0.145))
+            if((chara.getCenterX() + chara.getRadius()) < (Toolkit.getDefaultToolkit().getScreenSize().getWidth() - (Toolkit.getDefaultToolkit().getScreenSize().getWidth() * 0.145)) && ((chara.getCenterX() - chara.getRadius()) > (obs.getCenterX() + obs.getRadius()) || ((chara.getCenterY() + chara.getRadius()) < (obs.getCenterY() - obs.getRadius())) || ((chara.getCenterY() - chara.getRadius()) > (obs.getCenterY() + obs.getRadius())) || ((chara.getCenterX() + chara.getRadius()) < (obs.getCenterX() - obs.getRadius() - 1.0))))
             {
-                circle.setCenterX(circle.getCenterX() + 14.0);
+                chara.setCenterX(chara.getCenterX() + 1.0); // Changed movement to 1px at a time because it caused an issue with the obstacle
             }
           }
           else if (e.getCode() == KeyCode.UP)
           {
-              if(circle.getCenterY() - circle.getRadius() > (Toolkit.getDefaultToolkit().getScreenSize().getHeight() * 0.16))
+              if((chara.getCenterY() - chara.getRadius()) > (Toolkit.getDefaultToolkit().getScreenSize().getHeight() * 0.16) && ((chara.getCenterX() - chara.getRadius()) > (obs.getCenterX() + obs.getRadius()) || ((chara.getCenterY() + chara.getRadius()) < (obs.getCenterY() - obs.getRadius())) || ((chara.getCenterY() - chara.getRadius()) > (obs.getCenterY() + obs.getRadius() + 1.0)) || ((chara.getCenterX() + chara.getRadius()) < (obs.getCenterX() - obs.getRadius()))))
               {
-                circle.setCenterY(circle.getCenterY() - 14.0);
+                chara.setCenterY(chara.getCenterY() - 1.0); // Changed movement to 1px at a time because it caused an issue with the obstacle
               }
           }
           else if (e.getCode() == KeyCode.DOWN)
           {
-              if(circle.getCenterY() + circle.getRadius() < Toolkit.getDefaultToolkit().getScreenSize().getHeight() - (Toolkit.getDefaultToolkit().getScreenSize().getHeight() * 0.16))
+              if((chara.getCenterY() + chara.getRadius()) < (Toolkit.getDefaultToolkit().getScreenSize().getHeight() - (Toolkit.getDefaultToolkit().getScreenSize().getHeight() * 0.16)) && ((chara.getCenterX() - chara.getRadius()) > (obs.getCenterX() + obs.getRadius()) || ((chara.getCenterY() + chara.getRadius()) < (obs.getCenterY() - obs.getRadius() - 1.0)) || ((chara.getCenterY() - chara.getRadius()) > (obs.getCenterY() + obs.getRadius())) || ((chara.getCenterX() + chara.getRadius()) < (obs.getCenterX() - obs.getRadius()))))
               {
-                circle.setCenterY(circle.getCenterY() + 14.0);
+                chara.setCenterY(chara.getCenterY() + 1.0); // Changed movement to 1px at a time because it caused an issue with the obstacle
               }
           }
           else if(e.getCode() == KeyCode.SPACE)
